bec8d1fea7dbb866ef22c53ebb5eb17f93a4dca5
GabrielGrld <gbrlgrld@gmail.com>
2021-10-28 11:50:00 -0500

    create a list, add to the list when button is pressed

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require('axios');
 
 
 const app = express();
-
-var items = ["Buy food","Cook food","Eat food"];
-
+let items = ["Buy food","Cook food","Eat food"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-
-  var today = new Date();
-  var currentDay = today.getDay();
-
-  var options = {
+  let today = new Date();
+  let options = {
      weekday: 'long',
      day: 'numeric',
      month: 'long'  };
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render('list', { kindOfDay: day, newListItems: items });
-
+  res.render('list', {  kindOfDay: day, newListItems: items });
 });
 
 app.post('/', function(req, res){
@@ -31,6 +26,7 @@ app.post('/', function(req, res){
   items.push(item);
   res.redirect("/");
 });
+
 
 
 app.listen('3000', function() {
