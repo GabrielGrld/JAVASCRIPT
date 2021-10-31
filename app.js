6328d64583ccf276b741f5f4b767edc1ea403143
GabrielGrld <gbrlgrld@gmail.com>
2021-10-31 10:18:54 -0500

    creat a work list using same list.ejs template modify button value to redirect to work list and add item to workItems list

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const axios = require('axios');
 
 const app = express();
 let items = ["Buy food","Cook food","Eat food"];
+let workItems = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,14 +20,25 @@ app.get('/', function(req, res) {
      month: 'long'  };
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render('list', {  kindOfDay: day, newListItems: items });
+  res.render('list', {  listTitle: day, newListItems: items });
 });
 
 app.post('/', function(req, res){
-  let item = req.body.newItem;
+let item = req.body.newItem;
+if (req.body.list === "Work"){
+  workItems.push(item);
+  res.redirect("/work");
+}else{
   items.push(item);
   res.redirect("/");
+}
 });
+
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems });
+});
+
 
 
 
